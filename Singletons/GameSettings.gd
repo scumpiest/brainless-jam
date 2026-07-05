4b3c2caceca37565bb503d480f4ae84a9e9a12b8
Sora <9066690+Daggersoath@users.noreply.github.com>
2026-07-05 19:14:01 +0200

    Fix spacing in GameSettings

--- Singletons/GameSettings.gd
+++ Singletons/GameSettings.gd
@@ -1,11 +1,11 @@
 extends Node
 
-var MASTER_VOLUME : float = 1.0
-var MUSIC_VOLUME : float = 1.0
-var EFFECTS_VOLUME : float = 1.0
+var MASTER_VOLUME: float = 1.0
+var MUSIC_VOLUME: float = 1.0
+var EFFECTS_VOLUME: float = 1.0
 
 
-const _saveFilePath : String = "user://gameSettings.json"
+const _saveFilePath: String = "user://gameSettings.json"
 
 
 
@@ -26,7 +26,7 @@ class GodotProperty:
 
 
 func getProperties(filter: PropertyUsageFlags) -> Array[GodotProperty]:
-	var props : Array[GodotProperty] = []
+	var props: Array[GodotProperty] = []
 	for property in get_property_list():
 		var prop := GodotProperty.new(property)
 		if prop.usage & filter:
@@ -55,29 +55,29 @@ func loadFromFile() -> void:
 		print("Failed to load file: %s - %d" % [_saveFilePath, FileAccess.get_open_error()])
 		return
 
-	var saveDataStr : String = saveFile.get_as_text()
+	var saveDataStr: String = saveFile.get_as_text()
 	saveFile.close()
 	assert(saveDataStr)
 	
-	var saveData : Dictionary = JSON.parse_string(saveDataStr)
+	var saveData: Dictionary = JSON.parse_string(saveDataStr)
 	assert(saveData)
 	
-	for prop : GodotProperty in getProperties(PropertyUsageFlags.PROPERTY_USAGE_SCRIPT_VARIABLE):
+	for prop: GodotProperty in getProperties(PropertyUsageFlags.PROPERTY_USAGE_SCRIPT_VARIABLE):
 		if saveData.has(prop.name):
 			set(prop.name, saveData.get(prop.name, type_convert(null, prop.type)))
 			print("Game settings loaded: %s = %f" % [prop.name, get(prop.name)]) 
 
 
 func saveToFile() -> void:
-	var saveFile : FileAccess = FileAccess.open(_saveFilePath, FileAccess.WRITE)
+	var saveFile: FileAccess = FileAccess.open(_saveFilePath, FileAccess.WRITE)
 	assert(saveFile)
 	
-	var saveData : Dictionary = {}
-	for prop : GodotProperty in getProperties(PropertyUsageFlags.PROPERTY_USAGE_SCRIPT_VARIABLE):
+	var saveData: Dictionary = {}
+	for prop: GodotProperty in getProperties(PropertyUsageFlags.PROPERTY_USAGE_SCRIPT_VARIABLE):
 		saveData[prop.name] = JSON.stringify(get(prop.name))
 		print("Game setting saving: %s = %f" % [prop.name, get(prop.name)])
 	
-	var saveString : String = JSON.stringify(saveData)
+	var saveString: String = JSON.stringify(saveData)
 	assert(saveString)
 
 	saveFile.store_string(saveString)
