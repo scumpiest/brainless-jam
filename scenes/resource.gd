7915be40ddf124f7ee0cfd4f39eac4d1dddbfac4
Juca-5 <Joline.cezanne@gmx.de>
2026-07-09 23:36:23 +0200

    health bar on resource

--- scenes/resource.gd
+++ scenes/resource.gd
@@ -1,12 +1,23 @@
 extends Area3D
+class_name ResourceVein
 
 @export var health: int
 
+@onready var health_bar: ProgressBar = $SubViewport/ProgressBar
+var max_health: float
+var damage: float = 10
+
+func _ready() -> void:
+	max_health = health_bar.max_value
+	health_bar.value = max_health
+
+
 func _process(delta: float) -> void:
-	if health < 0:
+	if health_bar.value <= 0:
 		self.queue_free()
 
 func mine():
-	var current_health = health
-	health = current_health - 10
-	print("-10 hp")
+	#var current_health = health
+	#health = current_health - 10
+	var current_health_bar = health_bar.value
+	health_bar.value = current_health_bar - damage
